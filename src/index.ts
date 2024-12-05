7ba53591aabafb78d4df7297a035db47bfd50f8d
陈秋凡 <18782123479@xtransfer.cn>
2024-12-05 10:14:11 +0800

    feat: 优化入口文件配置

--- src/index.ts
+++ src/index.ts
@@ -5,27 +5,27 @@ import { findApiReferences } from './findApiReference';
 import { saveJson, clearObjectWithEmpty, caculateApiCount, importExcel, skippedImport } from './utils';
 import { prepare } from './prepare';
 
-const projectRoot = '/Users/xt02755/Desktop/Code/mfe-boss-customerfund/client'
-const entryFile = path.join(projectRoot, '/view/customerfund/WaterLevelBoard/WaterLevelBoard.vue'); 
-// const projectRoot = '/Users/chenqiufan/work/mfe-boss-operation-react/src'
-// const entryFile = path.join(projectRoot, '/pages/Notification/ReceiptTemplateManagement/index.tsx');
-// const projectRoot = '/Users/chenqiufan/work/bossfrontend/client'
-// const entryFile = path.join(projectRoot, 'view/businessApply/permissionApply/PermissionMannage.vue');
+const projectRoot = '/Users/chenqiufan/work/mfe-boss-customerfund-react/'
+const entryFilePath =  '/src/pages/FxManagement/FxTHB/Manage.tsx'
 
-// const projectRoot = '/Users/chenqiufan/work/mfe-boss-magpiebridge/client/'
-// const entryFile = path.join(projectRoot, 'view/customerservice/serviceOrder/components/SalesPlans/index.vue');
+
+const projectSrc = path.resolve(projectRoot, 'src')
+const projectClient = path.resolve(projectRoot, 'client')
+
+const finalProjectSrc = fs.existsSync(projectSrc) ? projectSrc : fs.existsSync(projectClient) ? projectClient : projectRoot
+const fullEntryFilePath = path.join(projectRoot, entryFilePath)
 
 // Step0: 预备工作
 prepare()
 
 // Step1: 解析所有文件，找出所有定义api的方法与文件的映射
-const [ apiCalls, unExportRequest ]= findAllApi(projectRoot);
+const [ apiCalls, unExportRequest ]= findAllApi(finalProjectSrc);
 
 // Step2: 从入口文件出发，找出所有调用的api
-const result = findApiReferences(entryFile, apiCalls, projectRoot)
+const result = findApiReferences(fullEntryFilePath, apiCalls, finalProjectSrc)
 // clearObjectWithEmpty(result)
 saveJson(path.resolve(__dirname, 'output', 'result.json'), result);
-importExcel(result, projectRoot, entryFile)
+importExcel(result, finalProjectSrc, fullEntryFilePath)
 // console.log(result)
 console.log(caculateApiCount(result))
 console.log('未导出的请求', unExportRequest)
